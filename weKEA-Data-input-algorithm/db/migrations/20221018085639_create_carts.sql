6a8473fb16e4ce727bddc1cd18dc6187e1cb1d15
이찬우 <scflcweric1201@gmail.com>
2022-10-21 18:17:24 +0900

    Change table carts;

--- weKEA-Data-input-algorithm/db/migrations/20221018085639_create_carts.sql
+++ weKEA-Data-input-algorithm/db/migrations/20221018085639_create_carts.sql
@@ -6,7 +6,7 @@ CREATE TABLE carts(
      created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
      updated_at TIMESTAMP NULL ON UPDATE CURRENT_TIMESTAMP,
      FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
-     FOREIGN KEY (product_option_id) REFERENCES product_options(id) ON DELETE CASCADE
+     FOREIGN KEY (product_option_id) REFERENCES product_options(id) ON DELETE CASCADE,
      CONSTRAINT carts_user_id_product_option_id_ukey UNIQUE (user_id, product_option_id)
 );
 
